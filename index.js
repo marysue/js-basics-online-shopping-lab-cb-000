422fea034018db6ff1927cb9737edf7459cd8440
Mary Lark <mary@marylark.com>
2018-11-23 01:29:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,70 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ // Use that passed-in string to create a new object 
+ //representing the item. The object should consist of two 
+ //key-value pairs : { itemName: name of the item, 
+ //itemPrice: price of the item, }. 
+ //As more items are added, the cart should start to look 
+ //something like this: [ { itemName:"bananas", itemPrice: 17 },
+ // { itemName:"pancake batter",itemPrice: 5 },
+ // { itemName:"eggs", itemPrice: 49 }].
+
+ //The price of each item should be a randomly-generated integer 
+ //between 1 and 100.
+//HINT: Look into Math.random() and Math.floor().
+//
+  var min = 1;
+  var max = 100;
+  var random = Math.floor(Math.random() * (max - min) + min);
+
+  var newObj = new Object( { itemName: item, itemPrice: random });
+
+  return newObj;
+    
 }
 
 function viewCart() {
+  
   // write your code here
+//The viewCart() function does not accept any arguments. 
+//It should loop over every item in your cart, returning 
+//the contents as one long, coherent statement in this format: 
+//In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
+//If the cart is empty, the function should instead return Your shopping cart is empty.
+//Note: Pay close attention to the syntax above. The returned statement should be a single 
+//sentence that begins with In your cart, you have, terminates in a period, and can 
+//assume the following shapes according to how many items the cart contains:
+//1 item — In your cart, you have bananas at $17.
+//2 items — In your cart, you have bananas at $17, and pancake batter at $5.
+//3+ items — In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
+//The total() function accepts no arguments, iterates through the cart array, and returns the current total value of the items in the cart.
+
+
+
+  var cart = getCart();
+  var retStr;
+  
+  if (cart.length === 0) {
+    // empty cart
+    retStr =  "Your shopping cart is empty."
+  } else {
+    for (let i=0; i < cart.length; i++){
+      //first item in cart - i=0;
+      if ((i === 0) && (cart.length === 1)) {
+        retStr = `In your cart, you have ${cart[i].itemName} at \$${cart[i].itemPrice}.` 
+      } else if ((i === 0) && (cart.length > 1)) {
+        retStr = `In your cart, you have ${cart[i].itemName} at \$${cart[i].itemPrice}, `
+      } else if (i < cart.length - 1) {
+        // mid string
+        retStr += `${cart[i].itemName} at \$${cart[i].itemPrice}, `
+      } else if (i === cart.length - 1) {
+        // last string
+        retStr += `and ${cart[i].itemName} at \$${cart[i].itemPrice}.`
+      }  //end else if;
+    }// end for
+  } // end else
+  return retStr;
 }
 
 function total() {
@@ -23,8 +83,17 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  //The removeFromCart() function accepts one argument, the name of the item that should be removed.
+//If the item is present in the cart, the function should remove the object from the cart and then return the updated cart.
+//HINT: Check each object's itemName value key to see if it matches the parameter, then remove it if it matches. You might find Array.prototype.splice() to be useful.
+//If the cart does not contain a matching item, the function should return That item is not in your cart.
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  //The placeOrder() function accepts one argument, a credit card number.
+//If no argument is received, the function should print out Sorry, we don't have a credit card on file for you..
+//If a card number is received, the function should
+//empty the cart array
+//return Your total cost is $71, which will be charged to the card 83296759. (where 71 is the value returned by total() and 83296759 is the credit card number passed to placeOrder())
 }
