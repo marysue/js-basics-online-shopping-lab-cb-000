c97da8513fa0c94d58b75cf0ac0234554ff63a09
Mary Lark <mary@marylark.com>
2018-11-23 02:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function placeOrder(cardNumber) {
       console.log(`Popping ${cart[i].itemName} (hopefully...)`);
       cart.pop();
     }
-    return `Your total cost is \$${total}, which will be charged to the card ${cardNumber}`
+    return `Your total cost is \$${total}, which will be charged to the card ${cardNumber}.`
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
